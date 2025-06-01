5ed5b483d8c1ae273eb992a6738a7abfe13c2f00
Rahat87942R <rumirahat63@gmail.com>
2025-06-01 21:04:38 +0600

    Fix import errors

--- util/formatPhoneNumber.js
+++ util/formatPhoneNumber.js
@@ -1,7 +1,9 @@
-export function formatPhoneNumber(phone) {
+function formatPhoneNumber(phone) {
   if (!phone) {
     return null;
   }
   const cleaned = phone.replace(/[\s()\-]/g, '');
   return `+1${cleaned}`;
 }
+
+export default formatPhoneNumber;
